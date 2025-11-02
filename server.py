dc88003cf6a09a6c20f138ff753f8bd6482bcc80
milhaniq <milhaniq@gmail.com>
2025-11-03 00:49:07 +0300

    Update server.py

--- server.py
+++ server.py
@@ -8,6 +8,6 @@ openai = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 @app.post("/api/chatkit/session")
 def create_chatkit_session():
     session = openai.chatkit.sessions.create(
-        workflow={"id": "wf_YOUR_WORKFLOW_ID_HERE"}
+        workflow={"id": "wf_68e5964d772481909ad033dd7a74a2020d5d3205250119fb"}
     )
     return {"client_secret": session.client_secret}
